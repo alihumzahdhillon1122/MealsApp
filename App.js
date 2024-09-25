6935a9561f1a518ad345924be740dc5248f3054c
alihumzahdhillon1122 <alihmzaeng@gmail.com>
2024-09-25 07:29:16 -0700

    complete the MealApp

--- App.js
+++ App.js
@@ -2,14 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
 
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverViewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoriteScreeen from './screens/FavoriteScreen';
 
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator screenOptions={{
+    headerStyle: { backgroundColor: '#351401' },
+    headerTintColor: 'white',
+    sceneContainerStyle: { backgroundColor: '#3f2f25' }
+  }}>
+    <Drawer.Screen name="Categories" component={CategoriesScreen} />
+    <Drawer.Screen name="Favorites" component={FavoriteScreeen} />
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -23,21 +38,17 @@ export default function App() {
           cardStyle: { backgroundColor: '#3f2f25' }
         }}>
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: 'All Categories',
+              // title: 'All Categories',
+              headerShown: false
 
             }} />
           <Stack.Screen
             name="MealsOverview"
             component={MealsOverViewScreen}
-          // options={(route, navigation) => {
-          //   const catId = route.params.categoryId;
-          //   return {
-          //     title: catId
-          //   }
-          // }}
+
           />
           <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
@@ -55,3 +66,4 @@ const styles = StyleSheet.create({
 });
 
 
+// 13;04
---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,7 @@
+import {Text} from 'react-native'
+function FavoriteScreeen (){
+    return <Text>the FavoriteScreeen</Text>
+}
+
+
+export default FavoriteScreeen;
