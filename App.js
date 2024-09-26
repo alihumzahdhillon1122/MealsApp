f2a03f01757994ec1f21638326958e7ff9c0c62d
alihumzahdhillon1122 <alihmzaeng@gmail.com>
2024-09-25 23:55:23 -0700

    complete the MealApp

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Ionicons } from '@expo/vector-icons'
 
 
 
@@ -19,10 +20,19 @@ function DrawerNavigator() {
   return <Drawer.Navigator screenOptions={{
     headerStyle: { backgroundColor: '#351401' },
     headerTintColor: 'white',
-    sceneContainerStyle: { backgroundColor: '#3f2f25' }
+    sceneContainerStyle: { backgroundColor: '#3f2f25' },
+    drawerContentStyle: { backgroundColor: '#3f2f25' },
+    drawerInactiveTintColor: 'white',
+    drawerActiveBackgroundColor: '#e4baa1'
   }}>
-    <Drawer.Screen name="Categories" component={CategoriesScreen} />
-    <Drawer.Screen name="Favorites" component={FavoriteScreeen} />
+    <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
+      title: 'All Categories',
+      drawerIcon: ({ color, size }) => (<Ionicons name="list" color={color} size={size} />)
+    }} />
+    <Drawer.Screen name="Favorites" component={FavoriteScreeen} options={{
+      drawerIcon: ({ color, size }) => (<Ionicons name="star" color={color} size={size} />)
+
+    }} />
   </Drawer.Navigator>
 }
 
@@ -42,15 +52,17 @@ export default function App() {
             component={DrawerNavigator}
             options={{
               // title: 'All Categories',
-              headerShown: false
+              headerShown: false,
+
 
             }} />
           <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverViewScreen}
+            name="MealsOverview" component={MealsOverViewScreen}
 
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{
+            title: 'About The Meal'
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
@@ -66,4 +78,3 @@ const styles = StyleSheet.create({
 });
 
 
-// 13;04
